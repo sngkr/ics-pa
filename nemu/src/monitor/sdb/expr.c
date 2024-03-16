9b5d6dd5bd395d2fbe96a95b2f7b456e32e7d187
tracer-ics2023 <tracer@njuics.org>
2024-03-16 20:17:06 +0800

    >  compile NEMU SA23225166 sngkr Linux sngkr-vm 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:17:06 up 1 day, 22:20,  2 users,  load average: 0.12, 0.10, 0.03

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -162,9 +162,9 @@ static bool make_token(char *e) {
           case MULTI:
           case NUM:
               tokens[nr_token].type = rules[i].token_type;
-            strncpy(tokens[nr_token].str, substr_start, substr_len);
-            Log("Type: %d  ",tokens[nr_token].type);
-            tokens[nr_token++].str[substr_len] = '\0';
+            strncpy(tokens[nr_token++].str, substr_start, substr_len);
+            Log("Type: %d  ",tokens[nr_token-1].type);
+            // tokens[nr_token++].str[substr_len] = '\0';
             int temp = 0;
             while(temp < substr_len){
               printf("%c", tokens[nr_token-1].str[temp++]);
